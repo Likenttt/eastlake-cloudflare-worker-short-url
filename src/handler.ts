2f4e724fb49574251c7d8d92755ca8abf89662bc
lichuanyi <chuanyi.work@gmail.com>
2023-04-14 01:34:09 +0800

    fix links not found

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,7 @@
-declare const LINKS_NAMESPACE: KVNamespace;
+import { KVNamespace } from "@cloudflare/workers-types";
+const linksKV = LINKS_PRE as unknown as KVNamespace;
+
+declare const LINKS_PRE: KVNamespace;
 
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url);
@@ -7,9 +10,13 @@ export async function handleRequest(request: Request): Promise<Response> {
   if (path === "shorten") {
     // Shorten the URL and save the mapping
     // Add your code to handle shortening and saving the URL
+
+    // Return an appropriate response after handling the URL shortening
+    // For example, a success message or the shortened URL itself
+    return new Response("Short URL created successfully", { status: 200 });
   } else {
     // Redirect the user to the full URL
-    const fullURL = await LINKS_NAMESPACE.get(path);
+    const fullURL = await linksKV.get(path);
     if (fullURL) {
       // Update click history
       // Add your code to update the click history
@@ -19,4 +26,7 @@ export async function handleRequest(request: Request): Promise<Response> {
       return new Response("URL not found", { status: 404 });
     }
   }
+
+  // Add a default return statement to cover any other cases
+  return new Response("Invalid request", { status: 400 });
 }
