e109bf35b7ab0fe8496b09816b60cfeb56f2c07d
lichuanyi <chuanyi.work@gmail.com>
2023-10-02 16:15:32 +0800

    add google analytics

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <>
+		<GoogleAnalytics trackPageViews />
+		<Component {...pageProps} />
+
+	</>
 }
