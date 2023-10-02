a29c938fa30b02810e784bfdf965706cb6b27077
lichuanyi <chuanyi.work@gmail.com>
2023-10-02 16:17:04 +0800

    fix ga tsx

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 export default function App({ Component, pageProps }: AppProps) {
-	return <>
-		<GoogleAnalytics trackPageViews />
-		<Component {...pageProps} />
-
-	</>
+	return (
+		<>
+			<GoogleAnalytics trackPageViews />
+			<Component {...pageProps} />
+		</>
+	);
 }
